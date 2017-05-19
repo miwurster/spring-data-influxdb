e92f42f6f1cbd4d4d91c87a29a45f1785dd2e2be
miwurster <miwurster@googlemail.com>
2017-05-19 11:48:54 +0200

    Added example to query data

--- src/test/java/org/springframework/data/influxdb/Application.java
+++ src/test/java/org/springframework/data/influxdb/Application.java
@@ -3,6 +3,10 @@ package org.springframework.data.influxdb;
 import com.google.common.collect.Lists;
 import java.util.concurrent.TimeUnit;
 import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,13 +15,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner
 {
+  private static Logger logger = LoggerFactory.getLogger(Application.class);
+
   @Autowired
   private InfluxDBTemplate<Point> influxDBTemplate;
 
   @Override
   public void run(final String... args) throws Exception
   {
+    // Create database...
     influxDBTemplate.createDatabase();
+
+    // Create some data...
     final Point p1 = Point.measurement("cpu")
       .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
       .tag("tenant", "default")
@@ -32,6 +41,11 @@ public class Application implements CommandLineRunner
       .addField("free", 1L)
       .build();
     influxDBTemplate.write(Lists.newArrayList(p1, p2));
+
+    // ... and query the latest data
+    final String q = "SELECT last(*) FROM cpu GROUP BY tenant";
+    final QueryResult queryResult = influxDBTemplate.getConnection().query(new Query(q, influxDBTemplate.getDatabase()));
+    logger.info(queryResult.toString());
   }
 
   public static void main(String[] args)
